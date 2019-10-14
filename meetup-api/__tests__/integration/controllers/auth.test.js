88310902f7052114ff51910c81514cc4207efadd
Evandro  Abreu de Oliveira <evandro@MacBook-Pro-de-Evandro.local>
2019-10-14 06:39:01 -0300

    initial settings

---
+++ meetup-api/__tests__/integration/controllers/auth.test.js
@@ -0,0 +1,35 @@
+import request from 'supertest';
+// import bcrypt from 'bcryptjs';
+import app from '../../../src/app';
+
+// import User from '../../src/app/models/User';
+import factory from '../../factories';
+
+import truncate from '../../util/truncate';
+
+describe('Auth', () => {
+  //clear database
+  beforeEach(async () => {
+    await truncate();
+  });
+
+  it('it should be possible login the app', async () => {
+    const user = await factory.attrs('User');
+
+    // save email and password
+    const { email, password } = user;
+
+    // create user
+    await request(app)
+      .post('/users')
+      .send(user);
+
+    // try acess the app
+    const response = await request(app)
+      .post('/sessions')
+      .send({ email, password });
+
+    expect(response.text).toContain('token');
+    expect(response.status).toBe(200);
+  });
+});
---
+++ meetup-api/src/server.js
@@ -0,0 +1,3 @@
+import app from './app';
+// coments
+app.listen(3000);
